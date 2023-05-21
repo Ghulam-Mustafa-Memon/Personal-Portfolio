9f5d3aec2473734df4c67a88cbadcec42a5a3c67
Ghulam-MustafaMemon <ghulammustafaattari06@gmail.com>
2023-05-21 19:32:02 +0500

    second data

---
+++ index.js
@@ -0,0 +1,49 @@
+
+// ______________ Skill1  ______________
+function pGenerator1(params) {
+    document.getElementById("skill1").innerHTML = `<p>provides a way to structure a web pages, by the set of tags and attributes.</p>`
+}
+
+function pRemover1(params) {
+    document.getElementById("skill1").innerHTML = ""
+}
+// ______________ Skill2  ______________
+function pGenerator2(params) {
+    document.getElementById("skill2").innerHTML = `<p>provides a variety of tools for styling, positioning, and formatting HTML and XML elements.</p>`
+}
+
+function pRemover2(params) {
+    document.getElementById("skill2").innerHTML = ""
+}
+// ______________ Skill3  ______________
+function pGenerator3(params) {
+    document.getElementById("skill3").innerHTML = `<p>provides a components of HTML, CSS, and JavaScript to make websites/applications responsive.</p>`
+}
+
+function pRemover3(params) {
+    document.getElementById("skill3").innerHTML = ""
+}
+// ______________ Skill4  ______________
+function pGenerator4(params) {
+    document.getElementById("skill4").innerHTML = `<p>Popular programming language for creating dynamic and interactive websites.</p>`
+}
+
+function pRemover4(params) {
+    document.getElementById("skill4").innerHTML = ""
+}
+// ______________ Skill5  ______________
+function pGenerator5(params) {
+    document.getElementById("skill5").innerHTML = `<p>library used for building (UIs) using a component-based approach.</p>`
+}
+
+function pRemover5(params) {
+    document.getElementById("skill5").innerHTML = ""
+}
+// ______________ Skill6  ______________
+function pGenerator6(params) {
+    document.getElementById("skill6").innerHTML = `<p>framework for (SSR) and building static and dynamic React-based web applications.</p>`
+}
+
+function pRemover6(params) {
+    document.getElementById("skill6").innerHTML = ""
+}
